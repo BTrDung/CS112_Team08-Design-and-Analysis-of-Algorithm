307f1f80aedc951022f0c100f6952478d2752af8
Dzung Bui Tri <45888776+BTrDung@users.noreply.github.com>
2021-03-20 12:03:39 +0700

    Update taobien_version1.py

--- Assignments/Week03/taobien_version1.py
+++ Assignments/Week03/taobien_version1.py
@@ -1,14 +1,12 @@
 n, k = map(int, input().split()) 
 
-dp = [0 for _ in range(0, k + 1)] 
-dp[0] = n 
+dp = [n for _ in range(0, k + 1)] 
+mod = 1000000007
 
 for i in range(1, k + 1, 1): 
-    sum = 0
+    sum = dp[i - 1]
     for j in range(0, i, 1): 
-        sum = sum + dp[j]
-    sum = sum + dp[i - 1]; 
-    
+        sum = (sum + dp[j]) % mod
     dp[i] = sum 
 
 print(dp[k])
