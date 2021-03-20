ef0ab21c32af4a6e4d8eb8c6de5338e26d9aa52e
Dzung Bui Tri <45888776+BTrDung@users.noreply.github.com>
2021-03-20 23:13:12 +0700

    Update taobien_version1.py

--- Assignments/Week03/taobien_version1.py
+++ Assignments/Week03/taobien_version1.py
@@ -1,21 +1,22 @@
-import numpy as np 
+def dot(matA, matB): 
+    mod = 1000000007
+    a = (matA[0][0]*matB[0][0])%mod + (matA[0][1]*matB[1][0])%mod # checked
+    b = (matA[0][0]*matB[0][1])%mod + (matA[0][1]*matB[1][1])%mod # checked
+    c = (matA[1][0]*matB[0][0])%mod + (matA[1][1]*matB[1][0])%mod # checked
+    d = (matA[1][0]*matB[0][1])%mod + (matA[1][1]*matB[1][1])%mod # checked
+    return [[a, b], [c, d]]
 
-def mod(mat): 
-    base = 1000000007
-    for i in mat: 
-        for j in i: 
-            j = j % base
-    return mat
 
-
-def fastPow(mat, number): 
-    if number == 0:
-         return np.array([[1, 0], [0, 1]])
-    mul = mod(fastPow(mat, number // 2))
+def fastPow(mat, number):
+    if number == 1: 
+        return mat
+    mult = fastPow(mat, number // 2)
     if number % 2 == 1: 
-        return mod(np.dot(mod(np.dot(mul, mul)), mat))
-    return mod(np.dot(mul, mul))
+        return dot(dot(mult, mult), mat)
+    return dot(mult, mult)
+
 
 n, k = map(int, input().split())
-result = np.reshape(mod(np.dot(fastPow(np.array([[1, 1], [1, 0]]), k * 2), np.array([[1], [0]]))), 2)
-print((result[0] * n) % 1000000007)
+matr = fastPow([[1, 1], [1, 0]], k * 2)
+print((matr[0][0] * n) % 1000000007)
+
