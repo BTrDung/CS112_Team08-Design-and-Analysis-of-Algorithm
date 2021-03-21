70823fdb903260058d89a7783d3c62d520a5f246
Dzung Bui Tri <45888776+BTrDung@users.noreply.github.com>
2021-03-21 10:14:10 +0700

    Update TAOBIEN.py

--- Assignments/Week03/TAOBIEN.py
+++ Assignments/Week03/TAOBIEN.py
@@ -1,22 +1,25 @@
-def dot(matA, matB): 
+def FFT(matA, matB): 
     mod = 1000000007
-    a = (matA[0][0]*matB[0][0])%mod + (matA[0][1]*matB[1][0])%mod # checked
-    b = (matA[0][0]*matB[0][1])%mod + (matA[0][1]*matB[1][1])%mod # checked
-    c = (matA[1][0]*matB[0][0])%mod + (matA[1][1]*matB[1][0])%mod # checked
-    d = (matA[1][0]*matB[0][1])%mod + (matA[1][1]*matB[1][1])%mod # checked
+    a = (matA[0][0]*matB[0][0])%mod + (matA[0][1]*matB[1][0])%mod 
+    b = (matA[0][0]*matB[0][1])%mod + (matA[0][1]*matB[1][1])%mod 
+    c = (matA[1][0]*matB[0][0])%mod + (matA[1][1]*matB[1][0])%mod 
+    d = (matA[1][0]*matB[0][1])%mod + (matA[1][1]*matB[1][1])%mod
     return [[a, b], [c, d]]
 
 
-def fastPow(mat, number):
-    if number == 1: 
-        return mat
-    mult = fastPow(mat, number // 2)
-    if number % 2 == 1: 
-        return dot(dot(mult, mult), mat)
-    return dot(mult, mult)
+def fastPow(radix, exponent):
+    if exponent == 1: 
+        return radix
+    base = fastPow(radix, exponent // 2)
+    if exponent % 2 == 1: 
+        return FFT(FFT(base, base), radix)
+    return FFT(base, base)
 
 
-n, k = map(int, input().split())
+#Input 
+n, k = map(int, input().split()) 
+
+#Calculate fibonanci 
 matr = fastPow([[1, 1], [1, 0]], k * 2)
-print((matr[0][0] * n) % 1000000007)
 
+print((matr[0][0] * n) % 1000000007)
